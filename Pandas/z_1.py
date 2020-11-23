b9e20c7c5477d10b7dd1ee298aa70e2fbbdd350a
DianaManko <72817907+DianaManko@users.noreply.github.com>
2020-11-23 22:14:31 +0300

    Delete z_1.py

--- Pandas/z_1.py
+++
@@ -1,7 +0,0 @@
-import csv
-ok = 'latvian','Lithuania','Estonia'
-with open('power.csv') as csvfile:
-    spamreader = list(csv.reader(csvfile, delimiter=','))[1:]
-    spamreaderok = list(filter(lambda a: a[0] == ok, spamreader))
-    c = sum(map(lambda x: int(x[2]), spamreaderok))
-    print(int(c))
