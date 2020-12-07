d2d5bd4638bc629839bbebb044aa82ce6350c9e6
DianaManko <72817907+DianaManko@users.noreply.github.com>
2020-12-07 20:36:05 +0300

    Update z_1.py

--- продвинутый пандас 2/z_1.py
+++ продвинутый пандас 2/z_1.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 data = pd.read_csv('ratings.csv')
-maxx = data['userid'].max() # сколько пользователей
-time [maxx]
+maxx = data['userId'].max() # сколько пользователей
+time = [0]*maxx
 for man in range(maxx): # идем по каждому пользователю
     
     ball=0
@@ -10,8 +10,8 @@ for man in range(maxx): # идем по каждому пользователю
     sort = data.query("userid = ",man)      
     
     if sort.rows >= 100:
-        min_time = sort[sort.userid == man and sort.timestamp == sort.timestamp.min()]
-        max_time = sort[sort.userid == man and sort.timestamp == sort.timestamp.max()]
+        min_time = sort[sort.userId == man and sort.timestamp == sort.timestamp.min()]
+        max_time = sort[sort.userId == man and sort.timestamp == sort.timestamp.max()]
         time[man] = max_time - min_time
         
 print ("средняя продолжительност ьжизни ", sum(time)/len(time))
