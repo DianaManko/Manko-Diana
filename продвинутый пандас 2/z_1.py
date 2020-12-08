43180b9805db0c363d603a01c955248b79994865
DianaManko <72817907+DianaManko@users.noreply.github.com>
2020-12-08 20:46:31 +0300

    Update z_1.py

--- продвинутый пандас 2/z_1.py
+++ продвинутый пандас 2/z_1.py
@@ -7,7 +7,7 @@ for man in range(maxx): # идем по каждому пользователю
     
     ball=0
     
-    sort = data.query("userid = ",man)      
+    sort = data.query("userid = @man")      
     
     if sort.rows >= 100:
         min_time = sort[sort.userId == man and sort.timestamp == sort.timestamp.min()]
